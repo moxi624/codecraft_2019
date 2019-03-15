5d2f2cfb3d34c12d967a2a63f1ff5527694b5eeb
taocan <tao9can8@vip.qq.com>
2019-03-15 22:57:23 +0800

    还原

--- CodeCraft-2019/src/read&process&output.py
+++ CodeCraft-2019/src/read&process&output.py
@@ -6,23 +6,23 @@ print("hahahaha")
 print("hahahaha")
 print("hahahaha")
 
-# def input_txt(file_address):
-#     with open(file_address, "r") as f:
-#         str = f.read()
-#         str = str.replace("(", "")
-#         str = str.replace(")", "")
-#         str = str.replace("\n", ",")
-#         str = str.lstrip('#qwertyuiopasdfghjklzxcvbnm,QWERTYUIOPASDFGHJKLZXCVBNM')
-#
-#     arr = str.split(',')
-#     Arr = np.array(arr)
-#     Arr = Arr.reshape(-1, 5)
-#     return Arr
-#
-#
-# car = input_txt("../config/car.txt")
-# road = input_txt("../config/road.txt")
-# cross = input_txt("../config/cross.txt")
+def input_txt(file_address):
+    with open(file_address, "r") as f:
+        str = f.read()
+        str = str.replace("(", "")
+        str = str.replace(")", "")
+        str = str.replace("\n", ",")
+        str = str.lstrip('#qwertyuiopasdfghjklzxcvbnm,QWERTYUIOPASDFGHJKLZXCVBNM')
+
+    arr = str.split(',')
+    Arr = np.array(arr)
+    Arr = Arr.reshape(-1, 5)
+    return Arr
+
+
+car = input_txt("../config/car.txt")
+road = input_txt("../config/road.txt")
+cross = input_txt("../config/cross.txt")
 
 #总路口数
 cross_number = len(cross)
@@ -38,8 +38,6 @@ for i in range(cross_number):
                 if cross[i][j] == cross[x][y]:
                     cross_adjacency_matrix[i][x] = 1
 
-print("hahahaha")
+
 print(cross_adjacency_matrix)
-print("dsfa")
-print("dsfa")
-print("dsfa")
+
