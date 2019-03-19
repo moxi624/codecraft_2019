5301f13b75768fb1a2eac5106211247d060cd39a
taocan <tao9can8@vip.qq.com>
2019-03-19 20:29:27 +0800

    增加频率最低路线，函数结构优化
    优化make_pt
    优化CodeCraft-2019

--- CodeCraft-2019/src/make_pt.py
+++ CodeCraft-2019/src/make_pt.py
@@ -1,9 +1,11 @@
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+
 G = nx.MultiDiGraph()
 
-#读取txt文件
+
+# 读取txt文件
 def input_txt(file_address):
     with open(file_address, "r") as f:
         str = f.read()
@@ -17,17 +19,18 @@ def input_txt(file_address):
     Arr = Arr.astype(int)
     return Arr
 
+
 road = input_txt("../1-map-training-1/road.txt").reshape(-1, 7)
 car = input_txt("../1-map-training-1/car.txt").reshape(-1, 5)
 cross = input_txt("../1-map-training-1/cross.txt").reshape(-1, 5)
 
-#计算每个节点中车的数量
+# 计算每个节点中车的数量
 tolm = []
 for i in cross:
     count = 0
     for j in car:
         if j[2] == i[0]:
-            count = count+1
+            count = count + 1
     tolm.append(str(count))
 # print(tolm)
 fromlm = []
@@ -35,20 +38,15 @@ for i in cross:
     count = 0
     for j in car:
         if j[1] == i[0]:
-            count = count+1
+            count = count + 1
     fromlm.append(str(count))
-#print(fromlm)
-# 设计节点编号
-nodelm = []
-m = 1
-for i in range(64):
-    nodelm.append(m)
-    m = m+1
+# print(fromlm)
 
-for i in range(64):
-    G.add_node(nodelm[i], bipartite=0, _type=tolm[i])
 
-#以添加边的形式画图
+for i in range(len(cross)):
+    G.add_node(i + 1, bipartite=0, _type=tolm[i])
+
+# 以添加边的形式画图
 for i in road:
     if i[6] == 1:
         G.add_edge(i[4], i[5])
@@ -62,8 +60,3 @@ plt.figure()
 edges = G.edges()
 nx.draw_spectral(G, edges=edges, labels=labels, node_color='y')
 plt.show()
-
-
-
-
-
