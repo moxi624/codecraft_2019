0254c32e057ff436a745af54dc4504995435cbfe
LM_LEMO <1050779162@qq.com>
2019-03-19 17:04:45 +0800

    画图：图中显示的是每个路口编号和车辆to的数量，因为from的都是平均分布，所以没有加进去。

--- CodeCraft-2019/src/make_pt.py
+++ CodeCraft-2019/src/make_pt.py
@@ -17,18 +17,38 @@ def input_txt(file_address):
     Arr = Arr.astype(int)
     return Arr
 
+road = input_txt("../1-map-training-1/road.txt").reshape(-1, 7)
+car = input_txt("../1-map-training-1/car.txt").reshape(-1, 5)
+cross = input_txt("../1-map-training-1/cross.txt").reshape(-1, 5)
+
+#计算每个节点中车的数量
+tolm = []
+for i in cross:
+    count = 0
+    for j in car:
+        if j[2] == i[0]:
+            count = count+1
+    tolm.append(str(count))
+# print(tolm)
+fromlm = []
+for i in cross:
+    count = 0
+    for j in car:
+        if j[1] == i[0]:
+            count = count+1
+    fromlm.append(str(count))
+#print(fromlm)
+# 设计节点编号
+nodelm = []
+m = 1
+for i in range(64):
+    nodelm.append(m)
+    m = m+1
+
+for i in range(64):
+    G.add_node(nodelm[i], bipartite=0, _type=tolm[i])
 
 #以添加边的形式画图
-
-road = input_txt("../1-map-training-2/road.txt").reshape(-1, 7)
-car = input_txt("../1-map-training-2/car.txt").reshape(-1, 5)
-#print(road)
-
-car_from = set()
-for item in car:
-    car_from.add(item[2])
-print(car_from)
-
 for i in road:
     if i[6] == 1:
         G.add_edge(i[4], i[5])
@@ -36,11 +56,12 @@ for i in road:
     else:
         G.add_edge(i[4], i[5])
 
+labels = dict((n, "(" + str(n) + "," + d['_type'] + ")") for n, d in G.nodes(data=True))
 
-nx.draw_spectral(G, with_labels=True, node_color='y')
-print(G.nodes())
+plt.figure()
+edges = G.edges()
+nx.draw_spectral(G, edges=edges, labels=labels, node_color='y')
 plt.show()
-
 
 
 
